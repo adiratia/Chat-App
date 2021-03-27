d41208ed2c934d5bee1c5e5c3291ddb6a9263964
adiratia <adir0305@gmail.com>
2021-03-27 13:12:32 +0300

    'update'

--- src/components/partials/Signup.js
+++ src/components/partials/Signup.js
@@ -7,7 +7,11 @@ class Signup extends Component{
         super(props);
         this.state={
             email:'',
-            password:''
+            password:'',
+            passwordAgain:'',
+            name:'',
+            username:'',
+            eroor:''
         }
     }
     render(){
@@ -20,17 +24,53 @@ class Signup extends Component{
                             onSubmit ={e=>{
                                 e.preventDefault();
                                 if(this.props.socket){
+                                    let empty =0;
+                                    Object.keys(this.state).map(key =>{
+                                        if(this.state[key]===''){
+                                            empty+=1;
+                                        }
+                                        
+                                    })
+                                   /* if(empty>0){
+                                        return this.setState({error:'All Fields Required'});
+                                    }
+                                    else{
+                                        if(this.state.password !== this.state.passwordAgain){
+                                            return this.setState({error:'Password Must Match'});
+
+                                        }
+                                    }*/
                                     this.props.socket.send(JSON.stringify({
                                         type: 'SIGNUP',
                                         data:{
                                             email : this.state.email,
-                                            password: this.state.password
+                                            password: this.state.password,
+                                            name: this.state.name,
+                                            username:this.state.username
                                         }
                                     }))
                                 }
                             }}
                         >
                             <h3>Register</h3>
+                            <div className="form-group">
+                                <label>Name</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    placeholder="Enter name"
+                                    value={this.state.name}
+                                    onChange={e=>this.setState({name:e.target.value})} />
+                            </div>
+                            <div className="form-group">
+                                <label>User name</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    placeholder="Enter user name"
+                                    value={this.state.username}
+                                    onChange={e=>this.setState({username:e.target.value})} />
+                            </div>
 
                             <div className="form-group">
                                 <label>Email</label>
@@ -41,7 +81,7 @@ class Signup extends Component{
                                     value={this.state.email}
                                     onChange={e=>this.setState({email:e.target.value})} />
                             </div>
-
+                                
                             <div className="form-group">
                                 <label>Password</label>
                                 <input 
@@ -51,11 +91,25 @@ class Signup extends Component{
                                     value={this.state.password}
                                     onChange={e=>this.setState({password:e.target.value})} />
                             </div>
+                            <div className="form-group">
+                                <label>Password Again</label>
+                                <input 
+                                    type="password"
+                                    className="form-control" 
+                                    placeholder="Enter password again"
+                                    value={this.state.passwordAgain}
+                                    onChange={e=>this.setState({passwordAgain:e.target.value})} />
+                            </div>
 
                             <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
                             <p className="forgot-password text-right">
                                 Already registered ? <Link to="/login">Login</Link>
                             </p>
+                            {this.state.error ?
+                                <p className="text-danger">{this.state.error}</p>
+                            :
+                            null
+                            }
                         </form>
                         </div>
                     </div>
